c7e18d74dc62b18c48eed317f6ec56bd99e4db38
parkjaewook1 <wodnr5050@gmail.com>
2024-07-15 12:07:48 +0900

    first commit

---
+++ src/main/java/com/example/solob/SolobApplication.java
@@ -0,0 +1,13 @@
+package com.example.solob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solob/SolobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
